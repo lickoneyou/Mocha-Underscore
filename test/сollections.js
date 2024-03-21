636743a32ecc3afc3fcc75462d5196dd297d42ef
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-03-21 08:47:52 +0300

    add find test

--- test/сollections.js
+++ test/сollections.js
@@ -9,4 +9,12 @@ describe("Collections", function () {
       [2, 3, 4]
     );
   });
+
+  it(`find _.find(list, predicate, [context]) Alias: detect source
+  Looks through each value in the list, returning the first one that passes a truth test (predicate), or undefined if no value passes the test. The function returns as soon as it finds an acceptable element, and doesn't traverse the entire list. predicate is transformed through iteratee to facilitate shorthand syntaxes.`, function () {
+    assert.deepEqual(
+      _.find([1, 2, 3, 4, 5, 6], (el) => el > 5),
+      6
+    );
+  });
 });
