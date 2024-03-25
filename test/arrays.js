90913a06cb0418e38e6800424af7d484d3df7209
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-03-25 10:59:27 +0300

    add without test

--- test/arrays.js
+++ test/arrays.js
@@ -6,4 +6,9 @@ describe("Arrays", function () {
     Returns a copy of the list with all falsy values removed. In JavaScript, false, null, 0, "", undefined and NaN are all falsy.`, function () {
     assert.deepEqual(_.compact([0, 1, false, 2, "", 3]), [1, 2, 3]);
   });
+
+  it(`without_.without(array, *values) source
+  Returns a copy of the array with all instances of the values removed.`, function () {
+    assert.deepEqual(_.without([0, 1, false, 2, 3], 1, false), [0, 2, 3]);
+  });
 });
