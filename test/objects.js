7e04b9c26394b16bad3d15f3fc216fc497d1e572
Kiryl Khomchanka <dickdurex265@gmail.com>
2024-03-25 11:25:20 +0300

    add isEmpty test

---
+++ test/objects.js
@@ -0,0 +1,11 @@
+const assert = require("assert");
+const _ = require("underscore");
+
+describe("Objects", function () {
+  it(`isEmpty_.isEmpty(collection) source
+  Returns true if collection has no elements. For strings and array-like objects _.isEmpty checks if the length property is 0. For other objects, it returns true if the object has no enumerable own-properties. Note that primitive numbers, booleans and symbols are always empty by this definition.`, function () {
+    assert.deepEqual(_.isEmpty([0, 1, false, 2, "", 3]), false);
+    assert.deepEqual(_.isEmpty([]), true);
+  });
+
+})
